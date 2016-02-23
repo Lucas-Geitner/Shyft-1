13a684926ae8ad6d22831e4e678b69fd1c366ce1
Quitterie Mathelin Moreaux <q.mathelin@gmail.com>
2016-02-23 11:26:30 +0100

    added form to poste creation

--- app/controllers/postes_controller.rb
+++ app/controllers/postes_controller.rb
@@ -1,16 +1,29 @@
 class PostesController < ApplicationController
   def new
+    @poste = Poste.new
   end
 
   def create
+    @poste = Poste.new(poste_params)
+    @poste.save
+    redirect_to postes_path
   end
 
   def edit
+    @poste = Poste.find(params[:id])
   end
 
   def update
+    @poste = Poste.find(params[:id])
   end
 
   def destroy
+    @poste = Poste.find(params[:id])
+    @poste.destroy
+    redirect_to(:back)
+  end
+
+  def poste_params
+    params.require(:poste).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+serveur = Poste.create(name: "serveur")
+salle = Poste.create(name: "salle")
+service = Poste.create(name: "Service")
