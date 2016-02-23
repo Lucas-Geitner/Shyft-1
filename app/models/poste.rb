b4592166a6daeb360bb3a47c9313c715b4bdbf02
massimo ercolani <massimo.ercolani28@github.com>
2016-02-23 11:46:58 +0100

    added has-many belongs-to relations to tables

--- app/models/poste.rb
+++ app/models/poste.rb
@@ -1,2 +1,3 @@
 class Poste < ActiveRecord::Base
+  has_many :shifts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :memberships
+  has_many :organisation_memberships
+  has_many :plannings
+  has_many :shifts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
