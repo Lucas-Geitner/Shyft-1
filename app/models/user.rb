74220a77cf07a327e63ddef476244a367abf960f
Quitterie Mathelin Moreaux <q.mathelin@gmail.com>
2016-02-24 11:48:11 +0100

    changed routes for plannings index, nested in shops

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :plannings
   has_many :shifts
   has_many :abilities
+  has_many :shops, through: :memberships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, except: :index
-  resources :plannings, except: :index
   resources :postes, except: :index
   resources :shifts, only: [:create, :update, :destroy]
   resources :memberships, only: :create
   resources :organisation_memberships, only: :create
   resources :organisations, only: :create
-  resources :shops, only: :create
-  get "shops/:shop_id/plannings" => 'plannings#index', as: :planning_index
+  resources :shops, only: :create do
+    resources :plannings, only: [:new, :create, :index]
+  end
+
+  resources :plannings, only: [:show, :edit, :update, :destroy]
+
 
   mount Attachinary::Engine => "/attachinary"
 end
