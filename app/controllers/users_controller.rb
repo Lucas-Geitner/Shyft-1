895507131c4c54b52074006122ffd91d3fa33745
massimo ercolani <massimo.ercolani28@github.com>
2016-02-24 18:21:33 +0100

    added link to new employees and cloudinary pdf upload function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :pdf)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :contract)
   end
 end
